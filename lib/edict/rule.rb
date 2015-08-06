446c88b9ef80b40a6dc93d44117e07302241f063
patriciaho <patricia.p.ho@gmail.com>
2015-08-05 22:55:21 -0700

    Add Rule

--- lib/edict/rule.rb
+++ lib/edict/rule.rb
@@ -1,4 +1,23 @@
 module Edict
   class Rule
+    include Calibrate::Configurable
+    include Calibrate::Configurable::DirectoryStructure
+
+    def initialize
+      setup_defaults
+      yield self
+      setup
+    end
+
+    def setup
+    end
+
+    def enact
+      check_required # from Calibrate
+      action
+    end
+
+    def action
+    end
   end
 end
